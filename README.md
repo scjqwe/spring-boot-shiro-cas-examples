6de143af98bda43da0854c4536bc125849ffb7b6
scjqwe <2290676174@qq.com>
2019-04-09 00:36:55 +0800

    添加README

---
+++ README.md
@@ -0,0 +1,6 @@
+# spring-boot-shiro-cas-examples
+Apache Shrio安全管理框架+CAS单点登陆框架
+
+### 建议参考文档
++ [shiro-cas学习](https://blog.csdn.net/zhoukun1008/article/details/73823839)
++ [学习源码](https://github.com/zhoukun6688/spring-shiro-cas)
--- src/main/resources/database/import.sql
+++ src/main/resources/database/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO `sys_user` (`uid`,`username`,`name`,`state`) VALUES ('1', 'suncj', '管理员', 0);
-INSERT INTO `sys_permission` (`id`,`available`,`name`,`parent_id`,`parent_ids`,`permission`,`resource_type`,`url`) VALUES (1,0,'用户管理',0,'0/','userInfo:view','menu','userInfo/userList');
-INSERT INTO `sys_permission` (`id`,`available`,`name`,`parent_id`,`parent_ids`,`permission`,`resource_type`,`url`) VALUES (2,0,'用户添加',1,'0/1','userInfo:add','button','userInfo/userAdd');
-INSERT INTO `sys_permission` (`id`,`available`,`name`,`parent_id`,`parent_ids`,`permission`,`resource_type`,`url`) VALUES (3,0,'用户删除',1,'0/1','userInfo:del','button','userInfo/userDel');
+INSERT INTO `sys_user` (`uid`,`username`,`name`,`state`) VALUES ('1', 'admin', '管理员', 0);
+INSERT INTO `sys_permission` (`id`,`available`,`name`,`parent_id`,`parent_ids`,`permission`,`resource_type`,`url`) VALUES (1,0,'用户管理',0,'0/','user:view','menu','user/list');
+INSERT INTO `sys_permission` (`id`,`available`,`name`,`parent_id`,`parent_ids`,`permission`,`resource_type`,`url`) VALUES (2,0,'用户添加',1,'0/1','user:add','button','user/add');
+INSERT INTO `sys_permission` (`id`,`available`,`name`,`parent_id`,`parent_ids`,`permission`,`resource_type`,`url`) VALUES (3,0,'用户删除',1,'0/1','user:del','button','user/del');
 INSERT INTO `sys_role` (`id`,`available`,`description`,`role`) VALUES (1,0,'管理员','admin');
 INSERT INTO `sys_role` (`id`,`available`,`description`,`role`) VALUES (2,0,'VIP会员','vip');
 INSERT INTO `sys_role` (`id`,`available`,`description`,`role`) VALUES (3,1,'test','test');
